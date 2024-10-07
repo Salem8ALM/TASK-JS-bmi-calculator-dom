c89b2856e6c2510a08a0dfa2fd4171792f7c534c
SALEM .. <salemsnsalmutairi@gmail.com>
2024-10-07 13:11:27 +0300

    salem

--- index.js
+++ index.js
@@ -8,4 +8,27 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  let BMI = weight / (height / 100) ** 2;
+  alert("the bmi is " + BMI);
+
+  if (BMI < 18.5) console.log("Underwight");
+  else if (BMI >= 18.5 && BMI <= 24.9) alert("Healthy weight");
+  else if (BMI >= 25.0 && BMI <= 29.9) alert("overweight");
+  else alert("above obesity");
+
+  if (age >= 19 && age <= 24 && BMI >= 19 && BMI <= 24) {
+    alert("Healthy");
+  } else if (age >= 25 && age <= 34 && BMI >= 20 && BMI <= 25) {
+    alert("Healthy");
+  } else if (age >= 35 && age <= 44 && BMI >= 21 && BMI <= 26) {
+    alert("Healthy");
+  } else if (age >= 45 && age <= 54 && BMI >= 22 && BMI <= 27) {
+    alert("Healthy");
+  } else if (age >= 55 && age <= 64 && BMI >= 23 && BMI <= 28) {
+    alert("Healthy");
+  } else if (age > 65 && BMI >= 24 && BMI <= 29) {
+    alert("Healthy");
+  } else {
+    alert("Not Healthy");
+  }
 }
